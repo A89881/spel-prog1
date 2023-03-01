7778413f806decea6318b52bccdf63c8680e8d9a
A89881 <abhay.sanatkumarmishra@elev.ga.ntig.se>
2023-03-01 15:47:04 +0100

    StartScreen Nearly Finished

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 karma = []
 allignments = ["good", "bad", "evil"]
+game_bool = True
+
 
 class StartScreen():
   def __init__(self, name, age, moralAllignment):
@@ -7,25 +9,27 @@ class StartScreen():
     self.age = age
     self.moralAllignment = moralAllignment
 
-  def StartMenu(self):
-    self.age = input(int("How old are you (in the game)?: "))
-    self.name =input(str("What is your name (in the game)?: "))
-    print(allignments)
-    self.moralAllignment = input(str("What allginment do you want? (choose from the above): ")).lower()
+def StartMenu():
+  StartScreen.age = int(input("How old are you (in the game)?: "))
+  StartScreen.name = str(input("What is your name (in the game)?: "))
+  print(allignments)
+  StartScreen.moralAllignment = str(input("What allginment do you want? (choose from the above): ")).lower()
 
-    try:
-      if self.moralAllignment == allignments[0]:
-        karma.append(10)
-      if self.moralAllignment == allignments[1]:
-        karma.append(0)
-      if self.moralAllignment == allignments[2]:
-        karma.append(-10)
-    
-    except:
-      print("You failed to understand the game")
+  if StartScreen.moralAllignment == allignments[0]:
+    karma.append(10)
+  if StartScreen.moralAllignment == allignments[1]:
+    karma.append(0)
+  if StartScreen.moralAllignment == allignments[2]:
+    karma.append(-10)
 
-    def StoryInformation(self):
-      
-      pass
+  if StartScreen.age > 99:
+    StartScreen.age = input(int("How old are you (in the game)?: "))
+  if len(name) > 10:
+    name = input(str("What is your name (in the game)?: "))  
+  
+s = StartScreen(0, 0, 0)
 
-    
+while game_bool == True:
+  StartMenu()
+  if s.StartMenu() == False:
+    game_bool = False
