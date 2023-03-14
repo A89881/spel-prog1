d60737975d6276b80ca5ba6a8e58c0dce3798989
mino225 <agnes.ohman@elev.ntig.se>
2023-03-14 15:11:18 +0100

    samlat ihop filerna i main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import time
 karma = []
 allignments = ["good", "bad", "evil"]
+minutes = 0
 meters = 0
-time = 0
-Start = True
-
+energy = 5
+choice = False
+bus = False
+#Start = True
 
 class StartScreen():
   def __init__(self, name, age, moralAllignment):
@@ -33,10 +36,106 @@ class StartScreen():
     print(f"Your Character; Name: {self.name} Age: {self.age} Moralallignment: {self.moralAllignment} Karma:{str(sum(karma))} ")
 
   def Text(self):
-    print(f"\n Hello {self.name}! You have a class in 15min, come to class in time or you will lose the game")
+    print(f"\nHello {self.name}! You have a class in 15min, come to class in time or you will lose the game")
   
 s = StartScreen(0, 0, 0)
 s.StartMenu()
 s.Text()
-while Start == True:
-  pass
+
+def checkFinish():
+   if minutes >= 15:
+      print("You stare down on you watch, and feel your heart dropping.")
+      time.sleep(2)
+      print("8.30.")
+      time.sleep(1)
+      print("Your class starts now.")
+      time.sleep(2)
+      #print("I'm sorry" s.name, "; you didn't make it.")
+   
+   elif meters >= 25:
+      print("You see the school slowly appearing in the horizon. You made it!")
+      time.sleep(2)
+      print("You start running as fast as you can. It feels like hours pass by, but somehow, it doe")
+
+print("You wake up to an screaming alarm.")
+print("...")
+
+print("You put on clothes and run down to the kitchen.")
+time.sleep(2)
+
+choice = True
+while choice == True:
+    print("Right when you're about to leave the house, you see a sandwich laying on the table. You hear your stomach growling as you're thinking about what you should do.")
+    time.sleep(2)
+    choiceAnswer = input("Do you eat the sandwich? \n [Yes] or [No] \n")
+
+    if choiceAnswer.lower() == "yes":
+        print("You walk up to the table as fast as you can. As you hear your stomach rumbling, you feel like you made the right decision.")
+        time.sleep(2)
+        print("You grab the sandwich and eat it slowly, while enjoying every single bite.")
+        time.sleep(2)
+        minutes += 1
+        energy += 1
+        choice = False
+
+    elif choiceAnswer.lower() == "no":
+        print("You chose to ignore your hunger and keep on walking out of the door.")
+        time.sleep(2)
+        choice = False
+
+    else:
+        print("Please choose yes or no.")
+        time.sleep(1)
+
+print("\n")
+print("The sun is shining. You slowly walk down the street, when you see a bus driving on the street in front of you.")
+time.sleep(2)
+print("You suddenly stop as you're watching the bus. Can you make it? Is it not worth it?")
+time.sleep(2)
+
+choice = True
+while choice == True:
+    choiceAnswer = input("Do you want to run and try and catch the bus? \n [Yes] or [No] \n")
+
+    if choiceAnswer.lower() == "yes":
+        print("You've got no time to lose! You start to run as fast as you can.")
+        time.sleep(2)
+        print("You get closer to the bus stop. The bus stops and people start to go aboard.")
+        time.sleep(2)
+        print("Your first day depends on this moment. While thinking that, you speed up.")
+        time.sleep(2)
+
+        if energy > 5:
+            print("You get to the bus and manages to jump on, right before the door closes")
+            print("You made it!")
+            minutes += 1
+            meters += 10
+            energy += 2
+            bus = True
+
+        else:
+            print("You get to the bus, just to see it driving away from you.")
+            time.sleep(2)
+            print("Exhausted from the running, you take a deep sigh for yourself.")
+            minutes += 1
+            meters += 5
+            energy -= 2
+
+        choice = False
+
+    elif choiceAnswer.lower() == "no":
+        print("You decide that it's not worth it, and continue to walk down the street.")
+        minutes += 1
+        meters += 5
+        energy -= 1
+
+        choice = False
+
+    else:
+        print("Please choose yes or no")
+
+if bus == True:
+   print("You sit on the bus, watching how the landscape is slowly moving.")
+
+print("\n")
+print("end of story")
---
+++ player.py
@@ -0,0 +1,42 @@
+karma = []
+allignments = ["good", "bad", "evil"]
+meters = 0
+time = 0
+Start = True
+
+
+class StartScreen():
+  def __init__(self, name, age, moralAllignment):
+    self.name = name
+    self.age = age
+    self.moralAllignment = moralAllignment
+
+  def StartMenu(self):
+    self.age = int(input("How old are you (in the game)?: "))
+    self.name = str(input("What is your name (in the game)?: "))
+    print(allignments)
+    self.moralAllignment = str(input("What allginment do you want? (choose from the above): ")).lower()
+
+    if self.moralAllignment == allignments[0]:
+      karma.append(10)
+    if self.moralAllignment == allignments[1]:
+      karma.append(0)
+    if self.moralAllignment == allignments[2]:
+      karma.append(-10)
+
+    if self.age > 99:
+      self.age = input(int("How old are you (in the game)?: "))
+    if len(self.name) > 10:
+      self.name = input(str("What is your name (in the game)?: "))  
+
+  def info(self):
+    print(f"Your Character; Name: {self.name} Age: {self.age} Moralallignment: {self.moralAllignment} Karma:{str(sum(karma))} ")
+
+  def Text(self):
+    print(f"\n Hello {self.name}! You have a class in 15min, come to class in time or you will lose the game")
+  
+s = StartScreen(0, 0, 0)
+s.StartMenu()
+s.Text()
+while Start == True:
+  pass
